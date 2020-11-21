3b8cfe7c663cb023807155a532e61b61c262378a
YoshinoriN <nayo-msoft@outlook.jp>
2020-11-21 11:42:30 +0900

    chore(lint): migrate tslint to typescript-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2019,
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json']
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  "rules": {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-return":"off",
+    "@typescript-eslint/ban-types": "off"
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { getObject } from "./processer";
 const config = hexo.config;
 
 // @ts-ignore
-hexo.extend.tag.register("config", (x) => {
+hexo.extend.tag.register("config", (x: any) => {
   const keys: Array<string> = x.toString().split(".");
   const depth: number = keys.length - 1;
   return getObject(config, 0, depth, keys);
